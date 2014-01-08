4676f1ab6d32b64864515a8cb88c4abd4d4f6405
Arbie Samong <phektus@gmail.com>
2014-01-08 21:20:20 +0800

    added markdown converter

--- mdblog.js
+++ mdblog.js
@@ -1,14 +1,27 @@
 if (Meteor.isClient) {
-  Template.editor.events({
-    'keypress textarea' : function () {
-	var raw = $('textarea#inputpad').val();
-	$('#outputpad').html(raw);
-    }
-  });
+    Template.editor.rendered = function() {
+	Meteor.setInterval(
+	    Template.editor.updatePad, 
+	    5 * 1000
+	);
+    };
+    
+    Template.editor.updatePad = function () {
+	console.log($('#inputpad').value);
+	$('#outputpad').html(markdown.toHTML(
+	    $('#inputpad').val()
+	));
+    };
+    
+    Template.editor.events({
+	'keyup textarea' : function() {
+	    Template.editor.updatePad();
+	}
+    });
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+    Meteor.startup(function () {
+	// code to run on server at startup
+    });
 }
