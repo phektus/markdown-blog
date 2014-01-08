d87d6cfa1060760d98bccca16dd8c2a1bea01a16
Arbie Samong <phektus@gmail.com>
2014-01-08 22:14:19 +0800

    reorg folders; added bootstrap and stylus

---
+++ client/app/mdblog.js
@@ -0,0 +1,20 @@
+
+Template.editor.rendered = function() {
+    Meteor.setInterval(
+	Template.editor.updatePad, 
+	5 * 1000
+    );
+};
+
+Template.editor.updatePad = function () {
+    $('#outputpad').html(markdown.toHTML(
+	$('#inputpad').val()
+    ));
+};
+
+Template.editor.events({
+    'keyup textarea' : function() {
+	Template.editor.updatePad();
+    }
+});
+
